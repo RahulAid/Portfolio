14e932457745a875bdbecd4cc91fbe69e2c2dae2
RahulAid <Rahuldeploys@gmail.com>
2025-06-18 21:50:13 +0530

    Updated Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Rahul_MERN_Updated_Resume.pdf";
+import pdf from "../../Assets/../Assets/Rahul_Resume_MERN_Stack.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
